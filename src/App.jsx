44f6bcc8f02b096b2dc3dc7f3ac95b2962d6c268
SudewSathnidu <158127340+SudewSathnidu@users.noreply.github.com>
2024-03-04 01:36:55 +0530

    added nav bar and patient location

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import Selectrole from "./Selectrole.jsx"
 import PatientApplication from "./PatientApplication.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Veridoc from "./sudew/Veridoc.jsx";
 function App() {
     return(
-        <Selectrole/>
+        <div>
+            <BrowserRouter>
+                  <Routes>
+                        <Route path="Location" element={<Location/>}> </Route>
+                        <Route path="/" element={<Veridoc/>}> </Route>
+                    
+                    </Routes>  
+            
+            
+            </BrowserRouter>
+        </div>
         
         // this is my comment
         
