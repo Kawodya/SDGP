f7ce5978703a46e05cb95e7112110bde8dd32ce6
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-13 13:55:29 +0530

    Added patient main page component
    added search jsx and css file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Verip from "./sudew/Verip.jsx";
 import Dapplication from "./dulnara/applicaton/Dapplication.jsx";
 import Papplication from "./dulnara/applicaton/Papplication.jsx";
 import Phapplication from "./dulnara/applicaton/Phapplication.jsx";
+import NextPage from "./dulnara/applicaton/NextPage.jsx";
 // import SelLoc from "./Patient/SelLoc.jsx";
 
 
@@ -23,6 +24,8 @@ function App() {
                         <Route path="/dapp" element={<Dapplication/>}> </Route>
                         <Route path="/papp" element={<Papplication/>}> </Route>
                         <Route path="/phapp" element={<Phapplication/>}> </Route>
+                        <Route path="/next" element={<NextPage/>}> </Route>
+
                         {/* <Route path="/selloc" element={<SelLoc/>}> </Route> */}
 
 
