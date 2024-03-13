cb755fadd559aba9c1ad3cac411e36885cf77e53
SudewSathnidu <158127340+SudewSathnidu@users.noreply.github.com>
2024-03-13 14:50:36 +0530

    Add comment to the import image

--- src/sudew/Veridoc.jsx
+++ src/sudew/Veridoc.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Verification from '../sudew/Verification';
-import veriimg from '../sudew/veriimg.jpeg';
+import veriimg from '../sudew/veriimg.jpeg';// import the verification image
 import './Veridoc.css';
 const Veridoc = () => {
   return (
     <>
     
-      <div className="veridoc-container">
+      <div className="veridoc-container"> 
       <img className='veridoc-logo' src="logomedlink.png" alt="main medlink logo" />
       <div className='veridoc-content'> 
       <img className='veridocwall' src={veriimg} alt="veri doc" />
