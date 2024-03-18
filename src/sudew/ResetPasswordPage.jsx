a50cf6fe2e8c9662e136f80823020804b6b3380d
SudewSathnidu <158127340+SudewSathnidu@users.noreply.github.com>
2024-03-18 17:56:50 +0530

    change some css

--- src/sudew/ResetPasswordPage.jsx
+++ src/sudew/ResetPasswordPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './../CSS/ResetPasswordPage.css'
+import '../sudew/ResetPasswordPage.css'
 
 const ResetPasswordPage = () => {
   const [password, setPassword] = useState('');
