2ec65fcabb843bada75d967c35f8ab8b2b88a915
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-22 16:18:12 +0530

    updated again user  card jsx

--- src/Pharmacy/userCard.jsx
+++ src/Pharmacy/userCard.jsx
@@ -1,43 +1,53 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import "./navbar.css"
-
+import React from 'react';
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+// import "./navbar.css";
 
 const name = "Sudew";
 const email = "sudew@gmail.com";
 
 const card = (
-        <div className='card' style={{display: 'flex',flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <Avatar
-            style={{textAlign: "center"}}
-            alt="Sudew"
-            src="/static/images/avatar/5.jpg"
-            sx={{ width: 100, height: 100}}
-            />
-            <Typography variant="h4" 
-            style={{textAlign: "center",
-                    textTransform: "capitalize",
-                    paddingTop: '20px',
-                    paddingBottom: '10px'}}>
-            {name}
-            </Typography>
-            <Typography variant="h6" 
-            style={{textAlign: "center",}}>
-                {email}
-            </Typography>
-        </div> 
+  <div
+    className="card"
+    style={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Avatar
+      style={{ textAlign: "center" }}
+      alt="Sudew"
+      src="/static/images/avatar/5.jpg"
+      sx={{ width: 100, height: 100 }}
+    />
+    <Typography
+      variant="h4"
+      style={{
+        textAlign: "center",
+        textTransform: "capitalize",
+        paddingTop: "20px",
+        paddingBottom: "10px",
+      }}
+    >
+      {name}
+    </Typography>
+    <Typography variant="h6" style={{ textAlign: "center" }}>
+      {email}
+    </Typography>
+  </div>
 );
 
 function UserCard() {
   return (
     <Button>
-            <Card variant="outlined">{card}</Card>
+      <Card variant="outlined">{card}</Card>
     </Button>
   );
 }
