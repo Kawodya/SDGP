68351b1c395d7ee3d1e15d88935c683e52fb7509
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-22 15:29:49 +0530

    Update Dapplication in login page

--- src/dulnara/applicaton/Dapplication.jsx
+++ src/dulnara/applicaton/Dapplication.jsx
@@ -1,95 +1,140 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import doctor from "../applicaton/doctor.jpeg";
-import { useNavigate } from 'react-router-dom';
-import { Link} from 'react-router-dom';
-import '../applicaton/Dapplication.css';
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../applicaton/Dapplication.css";
+import axiosClient from "../../axios-client";
+import { TextField } from "@mui/material";
 
 const Dapplication = () => {
-    const [Firstname, setFirstname] = useState('');
-    const [address, setAdress] = useState(''); 
-    const [lastname, setLastname] = useState(''); 
-    const [doctorId, setDoctorID] = useState('');
-    const [email, setEmail] = useState('');
-    const navigate = useNavigate();
+  const [Firstname, setFirstname] = useState("");
+  const [address, setAdress] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [doctorId, setDoctorID] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        // Validation logic
-        if (!Firstname || !lastname || !address || !doctorId || !email) {
-            alert('Please fill in all fields before submitting.');
-            return;
-        }
+  const handleLogin = () => {
+    // Validation logic
+    if (!Firstname || !lastname || !address || !doctorId || !email) {
+      alert("Please fill in all fields before submitting.");
+      return;
+    }
 
-        // If all fields are filled, navigate to another page
-        navigate('/photp');
+    const payload = {
+      first_name: Firstname,
+      last_name: lastname,
+      address: address,
+      uniqueId: doctorId,
+      email: email,
+      password: password,
+      type: "doctor",
     };
 
-    return (
-        <div className='Dapplication'>
-                
-                <div> 
-                    <img className='Medlinklog' src="logomedlink.png" alt="main medlink logo" />
-                    <div> 
-                        <p className='NewUserDapplication'>New user? </p>
-                        <Link to=' #'className='SignUpDapplication'> sign up </Link></div>
-                </div>
+    axiosClient
+      .post("/users", payload)
+      .then((res) => {
+        navigate("/dotp");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-            <h2 className='HeadDapplication'>Doctor Application</h2>
-            <img src={doctor} alt="doctor" />
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "150px",
+      }}
+    >
+      <img
+        style={{ position: "absolute", top: "20px", left: "50px" }}
+        className="Medlinklog"
+        src="logomedlink.png"
+        alt="main medlink logo"
+      />
+      <div style={{ position: "absolute", top: "20px", right: "50px" }}>
+        <span>Already have an Account?</span>
+        <Link to="/login" className="">
+          sign in
+        </Link>
+      </div>
 
-    
-            <form action='/' className='Dform'>
-                <input
-                    type="text"
-                    id="FirstnameDapplication"
-                    placeholder="First name"
-                    required
-                    value={Firstname}
-                    onChange={(e) => setFirstname(e.target.value)}
-                /><br />
-                <input
-                    type="text"
-                    id="lastnameDapplication"
-                    placeholder="last name"
-                    required
-                    value={lastname}
-                    onChange={(e) => setLastname(e.target.value)}
-                />
-                <br />
-                <input
-                    type="text"
-                    id="addressDapplication"
-                    placeholder="Address"
-                    required
-                    value={address}
-                    onChange={(e) => setAdress(e.target.value)}
-                />
-                <br />
-                <input
-                    type="text"
-                    id="doctorIDDapplication"
-                    placeholder="doctorId/Registerd No"
-                    required
-                    value={doctorId}
-                    onChange={(e) => setDoctorID(e.target.value)}
-                />
-                <br />
-                <input
-                    type="text"
-                    id="emailDapplication"
-                    placeholder="email or Contact number"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <button className='NextbtnDapplication' type="button" onClick={handleLogin}>
-                    Next
-                </button>
-                <label htmlFor="back button"> <b></b> </label> <br />
-     <button className='BackbtnDapplication' onClick={() => navigate ("/n")} title="back to previous page">Back </button>
-            </form>
-        </div>
-    );
+      <img style={{ flex: "1" }} src={doctor} alt="doctor" />
+      <div style={{ flex: "1.1" }}>
+        <h2 style={{ marginBottom: "30px", fontSize: "60px" }}>
+          Doctor Application
+        </h2>
+        <form style={{ margin: "0" }} action="/" className="">
+          <TextField
+            label="First name"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={Firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+          />
+          <TextField
+            label="Last name"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+          <TextField
+            label="Address"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={address}
+            onChange={(e) => setAdress(e.target.value)}
+          />
+          <TextField
+            label="DoctorId / Registerd No"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={doctorId}
+            onChange={(e) => setDoctorID(e.target.value)}
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div style={{ display: "flex", gap: "30px" }}>
+            <button style={{ margin: "0" }} type="button" onClick={handleLogin}>
+              Submit
+            </button>
+            <button
+              style={{ margin: "0" }}
+              onClick={() => navigate("/n")}
+              title="back to previous page"
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Dapplication;
