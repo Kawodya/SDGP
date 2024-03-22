e355b89abfb5693251fbf39314126557678b2afc
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-22 16:01:55 +0530

    updated css and next file jsx
    its about when select your own actor

--- src/dulnara/applicaton/NextPage.jsx
+++ src/dulnara/applicaton/NextPage.jsx
@@ -1,30 +1,98 @@
-import React from 'react'
-import { Link,useNavigate } from 'react-router-dom'
-import radioimg from '../applicaton/radioimg.jpeg'
-import '../applicaton/NextP.css'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import radioimg from "../applicaton/radioimg.jpeg";
+import "../applicaton/NextP.css";
 
-const  NextP  = () => {
-
-    const Navigate = useNavigate ();
+const NextP = () => {
+  const Navigate = useNavigate();
   return (
-    <div>
-       <div> 
-        <img className='Medlinklogo' src="logomedlink.png" alt="main medlink logo" />
-          
-         </div>
-      <img className='Roleimg' src= {radioimg} alt="" />
-      <h2 className='RoleHead'> Sign in as a ?</h2>
-    <label className='Roledoca' htmlFor="Doctor"> Doctor</label>
-     <button className='Roledocaselect' onClick={() => Navigate ("/dapplication")}> select </button>
-     <br />
-     <label className='RolePatient' htmlFor="Patient"> Patient</label>
-     <button className='RolePatientSelect' onClick={() => Navigate ("/papplication")}>select </button>
-     <br />
-     <label className='RolePharmacy' htmlFor="Pharmacy">Pharmacy </label>
-     <button className='RolePharmacySelect' onClick={() => Navigate ("/phapplication")}>select </button>
-      <br />    
-    </div>
-  )
-}
+    <>
+      <div style={{ position: "absolute", top: "20px", left: "20px" }}>
+        <img
+          className="Medlinklogo"
+          src="logomedlink.png"
+          alt="main medlink logo"
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "90vh",
+          gap: "50px",
+          margin: "20px",
+        }}
+      >
+        <img style={{ flex: "1" }} className="" src={radioimg} alt="" />
+        <div style={{ flex: "1" }}>
+          <h2 style={{ fontSize: "100px", marginBottom: "20px" }} className="">
+            Sign in as a ?
+          </h2>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "30px",
+              justifyContent: "space-between",
+              width: "400px",
+            }}
+          >
+            <label style={{ fontSize: "40px" }} htmlFor="Doctor">
+              Doctor
+            </label>
+            <button
+              style={{ margin: "0" }}
+              className=""
+              onClick={() => Navigate("/dapplication")}
+            >
+              select
+            </button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "30px",
+              justifyContent: "space-between",
+              width: "400px",
+            }}
+          >
+            <label style={{ fontSize: "40px" }} htmlFor="Doctor">
+              Patient
+            </label>
+            <button
+              style={{ margin: "0" }}
+              className=""
+              onClick={() => Navigate("/papplication")}
+            >
+              select
+            </button>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "30px",
+              justifyContent: "space-between",
+              width: "400px",
+            }}
+          >
+            <label style={{ fontSize: "40px" }} htmlFor="Doctor">
+              Pharmacy
+            </label>
+            <button
+              style={{ margin: "0" }}
+              className=""
+              onClick={() => Navigate("/phapplication")}
+            >
+              select
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default NextP
+export default NextP;
