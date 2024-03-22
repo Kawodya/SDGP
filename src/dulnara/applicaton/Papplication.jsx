6855680c2ad51db4da6cd90a02e16c69889e5f41
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-22 15:51:47 +0530

    updated the patient application jsx file

--- src/dulnara/applicaton/Papplication.jsx
+++ src/dulnara/applicaton/Papplication.jsx
@@ -1,91 +1,129 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import patient from "../applicaton/patient.png";
-import {useNavigate } from 'react-router-dom'
-import { Link } from 'react-router-dom';
-import '../applicaton/Papplication.css'
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../applicaton/Papplication.css";
+import { TextField } from "@mui/material";
 
 const Papplication = () => {
-    const [Firstname, setFirstname] = useState('');
-    const [address, setAdress] = useState(''); 
-    const [lastname, setLastname] = useState(''); 
-    const[email,setemail] =useState ('');
-    const navigate = useNavigate ();
-    
+  const [Firstname, setFirstname] = useState("");
+  const [address, setAdress] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogin = () => {
-        // Validation logic can be added here
-        if (!Firstname || !lastname || !address || !email) {
-            alert('Please fill in all fields before submitting.');
-            return;
-        }
-             navigate('/patientotp');
-        
-      };
+  const handleLogin = () => {
+    // Validation logic can be added here
+    if (!Firstname || !lastname || !address || !email || !password) {
+      alert("Please fill in all fields before submitting.");
+      return;
+    }
 
-    
+    const payload = {
+      first_name: Firstname,
+      last_name: lastname,
+      address: address,
+      email: email,
+      password: password,
+      type: "patient",
+    };
 
-    return (
-        <div className='Dapplication'>
+    axiosClient
+      .post("/users", payload)
+      .then((res) => {
+        navigate("/patientotp");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-                <div> 
-                    <header>
-                    <img className='LogoPapplication' src="logomedlink.png" alt="main medlink logo" />
-                    <div> 
-                        <p> </p>
-                        <Link to=' /patientotp' className='Papplicationsignup'> signin </Link> </div>
-                        </header> 
-                </div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "150px",
+      }}
+    >
+      <img
+        style={{ position: "absolute", top: "20px", left: "50px" }}
+        className="Medlinklog"
+        src="logomedlink.png"
+        alt="main medlink logo"
+      />
+      <div style={{ position: "absolute", top: "20px", right: "50px" }}>
+        <span>New user? </span>
+        <Link to=" #" className="">
+          sign up
+        </Link>
+      </div>
 
-
-            <h2 className='HeadPatientpagePapplication'>Patient Application</h2>
-            <img src={patient} alt="doctor" />
-            <form action='patientotp' className='Dform'>
-            <input
-            type="text"
-            id="FirstnamePapplication"
-            placeholder="First name"
+      <img style={{ flex: "1" }} src={patient} alt="patient" />
+      <div style={{ flex: "1.1" }}>
+        <h2 style={{ marginBottom: "30px", fontSize: "60px" }}>
+          Patient Application
+        </h2>
+        <form style={{ margin: "0" }} action="/" className="">
+          <TextField
+            label="First name"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
             value={Firstname}
             onChange={(e) => setFirstname(e.target.value)}
-            required
-            /><br/>
-               <input
-            type="text"
-            id="lastnamePapplication"
-            placeholder="last name"
+          />
+          <TextField
+            label="Last name"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
             value={lastname}
             onChange={(e) => setLastname(e.target.value)}
-            required
-            />
-            <br />
-            <input
-            type="text"
-            id="addressPapplication"
-            placeholder="Address"
+          />
+          <TextField
+            label="Address"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
             value={address}
             onChange={(e) => setAdress(e.target.value)}
-            required
-            />
-            <br />
-             <input
-            type="text"
-            id="emailPapplication"
-            placeholder="email or Contact number"
+          />
+          <TextField
+            label="Email"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
             value={email}
-            onChange={(e) => setemail(e.target.value)}
-            required
-            />
-            <br />
-           
-            <button className='NextbtnPapplication' type="button" onClick={handleLogin}>
-                    Next
-                </button>
-                <label htmlFor="back button"> <b></b> </label> <br />
-     <button className='BackPapplication' onClick={() => navigate ("/n")} title="back to previous page">Back </button>
-
-    
-            </form>
-        </div>
-    )
-}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            label="Password"
+            variant="outlined"
+            fullWidth={true}
+            style={{ marginBottom: "30px" }}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div style={{ display: "flex", gap: "30px" }}>
+            <button style={{ margin: "0" }} type="button" onClick={handleLogin}>
+              Submit
+            </button>
+            <button
+              style={{ margin: "0" }}
+              onClick={() => navigate("/n")}
+              title="back to previous page"
+            >
+              Back
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Papplication;
