2397fc9e5a4308c90d7ed82370c050dc1e54bbb3
Sunera2002 <143516854+Sunera2002@users.noreply.github.com>
2024-03-23 20:09:46 +0530

    Update Patientotp.jsx

--- src/sunera/Patientotp.jsx
+++ src/sunera/Patientotp.jsx
@@ -1,36 +1,56 @@
-import React from 'react'
-import OTPAuthentication from './OTPAuthentication'
-import { useNavigate } from 'react-router-dom';
-import '../sunera/Patientotp.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import OTPAuthentication from "./OTPAuthentication";
+import newdoc from "./newsdoc.jpg";
+import { useNavigate } from "react-router-dom";
+import "../sunera/Patientotp.css";
+import { Link } from "react-router-dom";
 const Patientotp = () => {
   const navigate = useNavigate();
 
   const handleNext = () => {
-    // Navigate to the next page based on your application's logic
-    // Example navigation, customize the URLs accordingly
-    navigate('/verip');
-  }
-
-
+    axiosClient
+      .post("/")
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <>
-    <div>
-      <img className='LogoPatientotp' src="logomedlink.png" alt="main medlink logo" />
-      <div> <Link to=' #' className='signinPatientotp'> sign up </Link></div>
-      <p className='NewUserPatientotp'>New user? </p>
-      
-    <div>
-    <h2 className='HeadPatientotp'>Verify your email address to create <br /> your new account </h2>
-    <p className='paraPatientotp'>An email with a verification code has been sent to <br /> <b>abcd@gmail.com</b> <br /> Enter the code here.</p>
-    </div>
-    <img className='PatientimgPatientotp' src="potpimg.jpeg" alt="potpimg" />
-    <OTPAuthentication handleNext={handleNext} />
-    </div>
-    
+      <div>
+        <img
+          style={{ position: "absolute", top: "20px", left: "50px" }}
+          className="Medlinklog"
+          src="logomedlink.png"
+          alt="main medlink logo"
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img style={{ flex: "1" }} src={newdoc} alt="newdoc" />
+          <div
+            style={{ display: "flex", flexDirection: "column", flex: "1.1" }}
+          >
+            <h2 className="">
+              Verify your email address to create <br /> your new account
+            </h2>
+            <p className="">
+              An email with a verification code has been sent to <br />
+              <b>your email</b> <br /> Enter the code here.
+            </p>
+            <OTPAuthentication handleNext={handleNext} />
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Patientotp
+export default Patientotp;
