bc5e833b9e98fb8104335c4b2116745f96ddb93e
Sunera2002 <143516854+Sunera2002@users.noreply.github.com>
2024-03-23 20:11:37 +0530

    Update Photp.jsx

--- src/sunera/Photp.jsx
+++ src/sunera/Photp.jsx
@@ -1,37 +1,45 @@
-import OTPAuthentication from "../components/OTPAuthentication"
-import { Link } from 'react-router-dom';
-import Otpimg from "../images/otp.jpeg"
-import { useNavigate } from 'react-router-dom';
-import '../sunera/Photp.css'
+import { Link } from "react-router-dom";
+import Otpimg from "./otp.jpeg";
+import { useNavigate } from "react-router-dom";
+import "../sunera/Photp.css";
+import OTPAuthentication from "./OTPAuthentication";
 
 const Photp = () => {
-    const navigate = useNavigate();
+  const handleNext = () => {};
 
-
-    const handleNext = () => {
-        // Navigate to the next page based on your application's logic
-        // Example navigation, customize the URLs accordingly
-        navigate('/veriph');
-      }
-
-    return(
-        <>
-        <div>
-        <img className='LogoPhotp' src="logomedlink.png" alt="main medlink logo" />
-        <p className='NewUserPhotp'>New user? </p>
-        <div>
-        <Link to=' #' className='SignUpPhotp'> sign up </Link>
+  return (
+    <>
+      <div>
+        <img
+          style={{ position: "absolute", top: "20px", left: "50px" }}
+          className="Medlinklog"
+          src="logomedlink.png"
+          alt="main medlink logo"
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img style={{ flex: "1" }} src={Otpimg} alt="newdoc" />
+          <div
+            style={{ display: "flex", flexDirection: "column", flex: "1.1" }}
+          >
+            <h2 className="">
+              Verify your email address to create <br /> your new account
+            </h2>
+            <p className="">
+              An email with a verification code has been sent to <br />
+              <b>your email</b> <br /> Enter the code here.
+            </p>
+            <OTPAuthentication />
+          </div>
         </div>
-        <div>
-        <h2 className="HeadpartPhotp">Verify your email address to create <br /> your new account </h2>
-        <p className="ppartPhotp">An email with a verification code has been sent to <br /> <b>abcd@gmail.com</b> <br /> Enter the code here.</p> 
-
-        </div>
-           <img className="wallimgPhotp" src= {Otpimg} alt="" />
-            <OTPAuthentication handleNext={handleNext}/>
-       </div>    
-        </>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default Photp;
