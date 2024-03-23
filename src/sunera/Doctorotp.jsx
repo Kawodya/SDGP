28894dde4d6bed2e01dbbc09d08da19c26e15052
Sunera2002 <143516854+Sunera2002@users.noreply.github.com>
2024-03-23 19:59:21 +0530

    Update Doctorotp.jsx

--- src/sunera/Doctorotp.jsx
+++ src/sunera/Doctorotp.jsx
@@ -1,37 +1,55 @@
-import React from 'react';
-import OTPAuthentication from './OTPAuthentication';
-import newdoc from './image/newsdoc.jpg';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import '../sunera/Doctorotp.css'
+import React from "react";
+import OTPAuthentication from "./OTPAuthentication";
+import newdoc from "./newsdoc.jpg";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "../sunera/Doctorotp.css";
+import axiosClient from "../axios-client";
 
 const Doctorotp = () => {
   const navigate = useNavigate();
 
   const handleNext = () => {
-    // Navigate to the next page based on your application's logic
-    // Example navigation, customize the URLs accordingly
-    navigate('/veridoc');
+    axiosClient
+      .post("/")
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <>
-    <div>
-
-      <img className='Pharmacyotpwall' src={newdoc} alt="newdoc" />
-      <p className='Newuserdocotp'>New user? </p>
       <div>
-      <img className='DotpMedlinklogo' src="logomedlink.png" alt="main medlink logo" />
-      <div>    <Link to=' #' className='signinDoctorotp'> sign up </Link></div>
-        <h2 className='HeadDoctorotp'>
-          Verify your email address to create <br /> your new account
-        </h2>
-        <p className='Paradocotp'>
-          An email with a verification code has been sent to <br />{' '}
-          <b>abcd@gmail.com</b> <br /> Enter the code here.
-        </p>
-      </div>
-      <OTPAuthentication handleNext={handleNext} />
+        <img
+          style={{ position: "absolute", top: "20px", left: "50px" }}
+          className="Medlinklog"
+          src="logomedlink.png"
+          alt="main medlink logo"
+        />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img style={{ flex: "1" }} src={newdoc} alt="newdoc" />
+          <div
+            style={{ display: "flex", flexDirection: "column", flex: "1.1" }}
+          >
+            <h2 className="">
+              Verify your email address to create <br /> your new account
+            </h2>
+            <p className="">
+              An email with a verification code has been sent to <br />
+              <b>your email</b> <br /> Enter the code here.
+            </p>
+            <OTPAuthentication handleNext={handleNext} />
+          </div>
+        </div>
       </div>
     </>
   );
