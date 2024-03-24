b4c329e7399842b266a732c0db98446a831d01bb
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-25 03:21:18 +0530

    Update navbar jsx
    It's use for the pharmacists and patients.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,23 +1,25 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import AppsRoundedIcon from '@mui/icons-material/AppsRounded';
-import logo from './image/logomed.png'
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import AppsRoundedIcon from "@mui/icons-material/AppsRounded";
+import logo from "./image/logomed.png";
+import { useNavigate } from "react-router-dom";
 
-const apps = ['App option 1', 'App option 2', 'App option 3']; // Add your app options here
+const apps = ["App option 1", "App option 2", "App option 3"]; // Add your app options here
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Navbar({ currentPage }) {
+  const navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
   const [anchorElApps, setAnchorElApps] = React.useState(null); // State for apps menu
@@ -42,42 +44,44 @@ function Navbar({ currentPage }) {
   };
 
   // Define text and color based on the currentPage
-  let pageText = '';
-  let pageColor = '';
-  if (currentPage === 'billing') {
-    pageText = 'Billing System';
-    pageColor = 'blue'; // Change to desired color
-  } else if (currentPage === 'inventory') {
-    pageText = 'Stock Management';
-    pageColor = 'green'; // Change to desired color
+  let pageText = "";
+  let pageColor = "";
+  if (currentPage === "billing") {
+    pageText = "Billing System";
+    pageColor = "#39A1DB"; // Change to desired color
+  } else if (currentPage === "inventory") {
+    pageText = "Stock Management";
+    pageColor = "green"; // Change to desired color
+  } else if (currentPage === "docin") {
+    pageText = "Prescription";
+    pageColor = "#39A1DB"; // Change to desired color
+  } else if (currentPage === "dashboard") {
+    pageText = "Pharmacist";
+    pageColor = "#7d17df"; // Change to desired color
   } else {
-    pageText = 'Patient';
-    pageColor = 'blue'; // Change to desired color
+    pageText = "Patient";
+    pageColor = "#39A1DB"; // Change to desired color
   }
 
   return (
-    <AppBar 
-      style={{ backgroundColor: 'white', opacity: 0.6 }}
-      position="fixed"
-    >
+    <AppBar style={{ backgroundColor: "white" }} position="fixed">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
             variant="h6"
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
             sx={{
-              display: 'flex',
-              alignItems: 'center',
-              fontFamily: 'monospace',
+              display: "flex",
+              alignItems: "center",
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            <img src={logo} style={{ width: '200px' }} alt="logo" srcset="" />
+            <img src={logo} style={{ width: "200px" }} alt="logo" srcset="" />
             <Typography
               variant="h6"
               noWrap
@@ -85,6 +89,8 @@ function Navbar({ currentPage }) {
               sx={{
                 ml: 2,
                 color: pageColor, // Change color dynamically
+                fontWeight: 600,
+                fontSize: "30px",
               }}
             >
               {pageText}
@@ -94,58 +100,45 @@ function Navbar({ currentPage }) {
           <Box sx={{ flexGrow: 1 }}></Box>
 
           <Box>
-            <Tooltip title="Apps">
-              <IconButton onClick={handleOpenAppsMenu} sx={{ p: 0 }}>
-                <AppsRoundedIcon />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-apps"
-              anchorEl={anchorElApps}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElApps)}
-              onClose={handleCloseAppsMenu}
-            >
-              {apps.map((app) => (
-                <MenuItem key={app} onClick={handleCloseAppsMenu}>
-                  <Typography textAlign="center">{app}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            
             <Tooltip title="User settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Pemy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
-            
+
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
             >
               {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
+                  <Typography
+                    onClick={
+                      setting === "Logout"
+                        ? () => {
+                            localStorage.removeItem("token");
+                            localStorage.removeItem("user");
+                            navigate("/login");
+                          }
+                        : null
+                    }
+                    textAlign="center"
+                  >
+                    {setting}
+                  </Typography>
                 </MenuItem>
               ))}
             </Menu>
