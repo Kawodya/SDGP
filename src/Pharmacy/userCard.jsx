da3bf7a8d17025cbadde79539d9a032a74bc3101
Dulnaraa <158497914+Dulnaraa@users.noreply.github.com>
2024-03-25 03:17:32 +0530

    updated user card
    create css

--- src/Pharmacy/userCard.jsx
+++ src/Pharmacy/userCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,46 +8,54 @@ import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
 // import "./navbar.css";
 
-const name = "Sudew";
-const email = "sudew@gmail.com";
-
-const card = (
-  <div
-    className="card"
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <Avatar
-      style={{ textAlign: "center" }}
-      alt="Sudew"
-      src="/static/images/avatar/5.jpg"
-      sx={{ width: 100, height: 100 }}
-    />
-    <Typography
-      variant="h4"
-      style={{
-        textAlign: "center",
-        textTransform: "capitalize",
-        paddingTop: "20px",
-        paddingBottom: "10px",
-      }}
-    >
-      {name}
-    </Typography>
-    <Typography variant="h6" style={{ textAlign: "center" }}>
-      {email}
-    </Typography>
-  </div>
-);
-
 function UserCard() {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user)
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+    }
+  }, []);
+
   return (
     <Button>
-      <Card variant="outlined">{card}</Card>
+      <Card variant="outlined">
+        <div
+          className="card"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            padding:"20px"
+          }}
+        >
+          <Avatar
+            style={{ textAlign: "center" }}
+            alt="Sudew"
+            src="/static/images/avatar/5.jpg"
+            sx={{ width: 100, height: 100 }}
+          />
+          <Typography
+            variant="h4"
+            style={{
+              textAlign: "center",
+              textTransform: "capitalize",
+              paddingTop: "20px",
+              paddingBottom: "10px",
+            }}
+          >
+            {name}
+          </Typography>
+          <Typography variant="h6" style={{ textAlign: "center" }}>
+            {email}
+          </Typography>
+        </div>
+      </Card>
     </Button>
   );
 }
